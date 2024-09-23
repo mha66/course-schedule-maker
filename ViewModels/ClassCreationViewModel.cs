e3a52611af9a2f9e1784d32f90eda52d816bde1c
mha66 <mharegaily@gmail.com>
2024-09-23 04:38:55 +0300

    refactor(ClassCreation): apply MVVM pattern to ClassCreationView

--- ViewModels/ClassCreationViewModel.cs
+++ ViewModels/ClassCreationViewModel.cs
@@ -1,12 +1,85 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CourseScheduleMaker.Views;
+using System.Windows.Controls;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.Windows;
+using CourseScheduleMaker.Models;
 
 namespace CourseScheduleMaker.ViewModels
 {
-    internal class ClassCreationViewModel
+    //TODO: add feat to fill all textboxes with last made course info
+    public class ClassCreationViewModel : ObservableObject
     {
+        //when changing the default value of SelectedSessionsNum, also change the default collection of SessionBoxes
+        public ObservableCollection<SessionTextBoxes> SessionBoxes { get; set; } = new ObservableCollection<SessionTextBoxes>() { new SessionTextBoxes(), new SessionTextBoxes() };
+
+        //TODO: put SessionsNum_SelectionChanged() logic inside setter and remove that method/command
+        public int SelectedSessionsNum { get; set; } = 2;
+        //SetProperty(ref _selectedSessionsNum, value);
+        public ObservableCollection<int> SessionsNums { get; set; } = new ObservableCollection<int>() { 1, 2, 3, 4 };
+
+        public string? CourseName { get; set; }
+        public string? CourseCode { get; set; }
+        public string? CourseGroup { get; set; }
+
+        public ICommand SessionsNum_SelectionChangedCmd { get; private set; }
+        public ICommand CreateCourse_ClickCmd { get; private set; }
+
+        public ClassCreationViewModel() 
+        {
+            SessionsNum_SelectionChangedCmd = new RelayCommand(SessionsNum_SelectionChanged);
+            CreateCourse_ClickCmd = new RelayCommand(CreateCourseBtn_Click);
+           
+        }
+        private void SessionsNum_SelectionChanged()
+        {
+            
+            int oldCount = SessionBoxes.Count, newCount = SelectedSessionsNum;
+
+            if (oldCount < newCount)
+            {
+                while (oldCount != newCount)
+                {
+                    var newSessionBoxes = new SessionTextBoxes() { Uid = "session" + SessionTextBoxesViewModel.Count };
+                    SessionTextBoxesViewModel.Count++;
+                    oldCount++;
+                    SessionBoxes.Add(newSessionBoxes);
+                }
+            }
+            else if (oldCount > newCount)
+            {
+
+                while (oldCount != newCount)
+                {
+                    SessionBoxes.RemoveAt(SessionBoxes.Count - 1);
+                    SessionTextBoxesViewModel.Count--;
+                    oldCount--;
+                }
+
+            }
+        }
+        private void CreateCourseBtn_Click()
+        {
+
+            Course course = Course.ExistingOrNew(new Course(CourseName!, CourseCode!));
+            Group group = Group.ExistingOrNew(new Group(CourseGroup!, course));
+            Class classes = new Class(course, group);
+            foreach (SessionTextBoxes sessionBox in SessionBoxes)
+            {
+                var sessionViewModel = sessionBox.DataContext as SessionTextBoxesViewModel;
+                classes.AddSession(new Session(sessionViewModel!.SessionKind, sessionViewModel.Instructor!,
+                    sessionViewModel.Day, sessionViewModel.Period));
+            }
+            DBSource.InsertData(classes);
+            MessageBox.Show("Courses and groups are updated!");
+        }
     }
 }
--- Views/ClassCreationView.xaml.cs
+++ Views/ClassCreationView.xaml.cs
@@ -22,89 +22,14 @@ namespace CourseScheduleMaker.Views
     /// 
 
 
-    //TODO: add feat to fill all textboxes with last made course info
+
     public partial class ClassCreationView : Window
     {
-        public List<SessionTextBoxes> SessionBoxes { get; set; } = new List<SessionTextBoxes>();
-        //public SessionType Kind { get; set; } = SessionType.Lec;
+       
         public ClassCreationView()
         {
             InitializeComponent();
         }
 
-        private void SessionsNum_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            ComboBox comboBox = (sender as ComboBox)!;
-            int oldCount = SessionTextBoxesViewModel.Count, newCount = 0;
-            newCount = int.Parse(comboBox.SelectedValue.ToString()!);
-
-            if (oldCount < newCount)
-            {
-                while (oldCount != newCount)
-                {
-                    var newSessionBoxes = new SessionTextBoxes() {Uid = "session" + SessionTextBoxesViewModel.Count };
-                    SessionTextBoxesViewModel.Count++;
-                    oldCount++;
-                    creationGrid.Children.Add(newSessionBoxes);
-                    SessionBoxes.Add(newSessionBoxes);
-                   
-                    Grid.SetRow(newSessionBoxes, (oldCount < 3) ? 2 : 3);
-                    Grid.SetColumn(newSessionBoxes, (oldCount + 1) % 2);
-                }
-            }
-            else if (oldCount > newCount)
-            {
-
-                while (oldCount != newCount)
-                {
-                    int childrenCount = creationGrid.Children.Count;
-                    //to avoid removing the Create button
-                    creationGrid.Children.RemoveAt(
-                        (creationGrid.Children[childrenCount - 1].Uid == "createBtn") ? childrenCount - 2 : childrenCount - 1);
-                    SessionBoxes.RemoveAt(SessionBoxes.Count - 1);
-                    SessionTextBoxesViewModel.Count--;
-                    oldCount--;
-                }
-
-            }
-        }
-
-
-        private void CreateCourseBtn_Click(object sender, RoutedEventArgs e)
-        {
-
-            Course course = Course.ExistingOrNew(new Course(courseName.Text, courseCode.Text));
-            Group group = Group.ExistingOrNew(new Group(courseGroup.Text, course));
-            Class classes = new Class(course, group);
-            foreach (SessionTextBoxes sessionBox in SessionBoxes)
-            {
-                var sessionViewModel = sessionBox.DataContext as SessionTextBoxesViewModel;
-                classes.AddSession(new Session(sessionViewModel!.SessionKind, sessionViewModel.Instructor!,
-                    sessionViewModel.Day, sessionViewModel.Period));
-            }
-            DBSource.InsertData(classes);
-            //MainWindow.UpdateCourseGroupViews();
-            //foreach (Window window in Application.Current.Windows)
-            //{
-            //    //if (window.GetType() == typeof(MainWindow))
-            //    //{
-            //    //    var mainWindow = window as MainWindow;
-            //    //    foreach (var oldClass in MainWindow.ClassesView!)
-            //    //    {
-            //    //        if (oldClass.Course.Id == course.Id)
-            //    //        {
-            //    //            //oldClass.Course.AddGroup(group);
-
-            //    //            MainWindow.ClassesView.Remove(oldClass);
-            //    //            MainWindow.ClassesView.Add(oldClass);
-            //    //        }
-            //    //    }
-            //    //        mainWindow!.addedCourses.Items.Refresh();
-                    
-            //    //    break;
-            //    //}
-            //}
-            MessageBox.Show("courses and groups are updated!");
-        }
     }
 }
